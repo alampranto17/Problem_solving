c7bab8c26ada77b23aa0bd8de2902c3f03d42993
Alam_Pranto <alampranto17@gmail.com>
2025-04-20 23:08:07 +0600

    update for pc restarting

---
+++ Python Problem/StringProblem1.py
@@ -0,0 +1,27 @@
+# 1.Create a string made of the first, middle and last character
+from pickle import APPEND
+
+# string=input("Enter your string?")
+#
+# res=string[0];
+#
+# res =res+ string[len(string) // 2];
+#
+# res=res +string[-1];
+#
+# print(res)
+
+# 2. Append new string in the middle of a given string
+
+s1= "hello";
+
+index=len(s1)/2;
+
+s2= "alam"
+
+s3=s1.ap
+
+
+
+
+
